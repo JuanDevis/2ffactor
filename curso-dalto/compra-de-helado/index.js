5b3f27b7bb805e52bf6c2e2eb1509dd856901546
John Steven Gonzalez <contactoxtiv@gmail.com>
2024-01-09 15:32:00 +0100

    init

--- curso-dalto/compra-de-helado/index.js
+++ curso-dalto/compra-de-helado/index.js
@@ -1,80 +1,80 @@
-// dineroCofla = prompt("cuanto dinero Cofla? ")
-// dineroRoberto = prompt("cuanto dinero Roberto? ")
-// dineroPedro = prompt("cuanto dinero Pedro? ")
+dineroCofla = prompt("cuanto dinero Cofla? ")
+dineroRoberto = prompt("cuanto dinero Roberto? ")
+dineroPedro = prompt("cuanto dinero Pedro? ")
 
-// if (dineroCofla >= 0.6 && dineroCofla < 1) {
-//     alert("compra el helado de agua")
-// }
-// else if (dineroCofla >= 1 && dineroCofla < 1.6) {
-//     alert("compra el helado de crema")
-// }
-// else if (dineroCofla >= 1.6 && dineroCofla < 1.7) {
-//     alert("compra el helado de crema")
-// }
-// else if (dineroCofla >= 1.7 && dineroCofla < 1.8) {
-//     alert("compra el helado de crema")
-// }
-// else if (dineroCofla >= 1.8 && dineroCofla < 2.9) {
-//     alert("compra el helado de crema")
-// }
-// else if (dineroCofla >= 2.9) {
-    
-// } else {
-//     alert("lo siento, pobre de mierda , no te alcanza")
-// }
-
-// if (dineroRoberto >= 0.6 && dineroRoberto < 1) {
-//     alert("compra el helado de agua")
-// }
-// else if (dineroRoberto >= 1 && dineroRoberto < 1.6) {
-//     alert("compra el helado de crema")
-// }
-// else if (dineroRoberto >= 1.6 && dineroRoberto < 1.7) {
-//     alert("compra el helado de crema")
-// }
-// else if (dineroRoberto >= 1.7 && dineroRoberto < 1.8) {
-//     alert("compra el helado de crema")
-// }
-// else if (dineroRoberto >= 1.8 && dineroRoberto < 2.9) {
-//     alert("compra el helado de crema")
-// }
-// else if (dineroRoberto >= 2.9) {
-    
-// } else {
-//     alert("lo siento, pobre de mierda , no te alcanza")
-// }
-
-
-// if (dineroPedro >= 0.6 && dineroPedro < 1) {
-//     alert("compra el helado de agua")
-// }
-// else if (dineroPedro >= 1 && dineroPedro < 1.6) {
-//     alert("compra el helado de crema")
-// }
-// else if (dineroPedro >= 1.6 && dineroPedro < 1.7) {
-//     alert("compra el helado de crema")
-// }
-// else if (dineroPedro >= 1.7 && dineroPedro < 1.8) {
-//     alert("compra el helado de crema")
-// }
-// else if (dineroPedro >= 1.8 && dineroPedro < 2.9) {
-//     alert("compra el helado de crema")
-// }
-// else if (dineroPedro >= 2.9) {
-    
-// } else {
-//     alert("lo siento, pobre de mierda , no te alcanza")
-// }
-
-let parrafo = document.querySelector(".parrafo-prueba")
-
-const definirCompra = (nombre)=>{
-    let dinero = prompt(`Dinero de ${nombre}`);
-    
-    if (dinero >= 0.6 && dinero < 1) {
-        parrafo.innerHTML = "Eres pobre";
-        parrafo.style.background = "#0000FF"
-    } else {
-        parrafo.innerHTML = "Eres millo";
-    }
+if (dineroCofla >= 0.6 && dineroCofla < 1) {
+    alert("compra el helado de agua")
 }
+else if (dineroCofla >= 1 && dineroCofla < 1.6) {
+    alert("compra el helado de crema")
+}
+else if (dineroCofla >= 1.6 && dineroCofla < 1.7) {
+    alert("compra el helado de crema")
+}
+else if (dineroCofla >= 1.7 && dineroCofla < 1.8) {
+    alert("compra el helado de crema")
+}
+else if (dineroCofla >= 1.8 && dineroCofla < 2.9) {
+    alert("compra el helado de crema")
+}
+else if (dineroCofla >= 2.9) {
+    
+} else {
+    alert("lo siento, pobre de mierda , no te alcanza")
+}
+
+if (dineroRoberto >= 0.6 && dineroRoberto < 1) {
+    alert("compra el helado de agua")
+}
+else if (dineroRoberto >= 1 && dineroRoberto < 1.6) {
+    alert("compra el helado de crema")
+}
+else if (dineroRoberto >= 1.6 && dineroRoberto < 1.7) {
+    alert("compra el helado de crema")
+}
+else if (dineroRoberto >= 1.7 && dineroRoberto < 1.8) {
+    alert("compra el helado de crema")
+}
+else if (dineroRoberto >= 1.8 && dineroRoberto < 2.9) {
+    alert("compra el helado de crema")
+}
+else if (dineroRoberto >= 2.9) {
+    
+} else {
+    alert("lo siento, pobre de mierda , no te alcanza")
+}
+
+
+if (dineroPedro >= 0.6 && dineroPedro < 1) {
+    alert("compra el helado de agua")
+}
+else if (dineroPedro >= 1 && dineroPedro < 1.6) {
+    alert("compra el helado de crema")
+}
+else if (dineroPedro >= 1.6 && dineroPedro < 1.7) {
+    alert("compra el helado de crema")
+}
+else if (dineroPedro >= 1.7 && dineroPedro < 1.8) {
+    alert("compra el helado de crema")
+}
+else if (dineroPedro >= 1.8 && dineroPedro < 2.9) {
+    alert("compra el helado de crema")
+}
+else if (dineroPedro >= 2.9) {
+    
+} else {
+    alert("lo siento, pobre de mierda , no te alcanza")
+}
+
+// let parrafo = document.querySelector(".parrafo-prueba")
+
+// const definirCompra = (nombre)=>{
+//     let dinero = prompt(`Dinero de ${nombre}`);
+    
+//     if (dinero >= 0.6 && dinero < 1) {
+//         parrafo.innerHTML = "Eres pobre";
+//         parrafo.style.background = "#0000FF"
+//     } else {
+//         parrafo.innerHTML = "Eres millo";
+//     }
+// }
